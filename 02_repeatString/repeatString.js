2f11b9be5b5e96bd972958a0662cd93de2a2e04a
Pablo Miralles <pablo@mowomo.com>
2023-12-26 20:55:57 +0100

    Complete Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
 
+	if (num >= 0) {
+		let repeatStringResult = "";
+		for (i = 0; i < num; i++) {
+			repeatStringResult += string;
+		}
+		return repeatStringResult;
+	} else {
+		return 'ERROR';
+	}
 };
 
 // Do not edit below this line
