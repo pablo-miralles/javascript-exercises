1cba4db7467db2b0045d29f5f8a2be1369329a94
Pablo Miralles <pablo@mowomo.com>
2023-12-29 00:31:39 +0100

    Complete Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (givenArray, ...elementsToRemove) {
 
+	const resultArray = givenArray.filter(function (item) {
+		return !elementsToRemove.includes(item);
+	});
+
+	return resultArray;
 };
 
 // Do not edit below this line
