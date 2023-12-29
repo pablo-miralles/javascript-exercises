b0f50be6b6f8a65f807b9c5fe742cf4196b8bdd2
Pablo Miralles <pablo@mowomo.com>
2023-12-29 02:51:02 +0100

    Complete Exercise 5

---
+++ 05_sumAll/notes.md
@@ -0,0 +1,11 @@
+firstNumber: 1
+lasNumber 4
+
+loop 1
+	0 + 1 = 1
+loop 2
+	1 + 2 = 3
+loop 3
+	3 + 3 = 6
+loop 4
+	6 + 4 = 10
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, finalNumber) {
+	if (typeof firstNumber !== "number" || typeof finalNumber !== "number") {
+		return 'ERROR';
+	} else if (firstNumber < 0 || finalNumber < 0) {
+		return 'ERROR';
+	}
+
+	let finalResult;
+
+	if (firstNumber > finalNumber) {
+		for (finalResult = 0; finalNumber <= firstNumber; finalNumber++) {
+			finalResult += finalNumber;
+		}
+	} else {
+		for (finalResult = 0; firstNumber <= finalNumber; firstNumber++) {
+			finalResult += firstNumber;
+		}
+	}
+
+	return finalResult;
 
 };
 
