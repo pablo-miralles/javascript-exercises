40b8d5c6c935fe146e385e03337853f69d821b25
Pablo Miralles <pablo@mowomo.com>
2024-04-28 20:26:14 +0200

    Completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = item => {
+	var punctuation = /[\s\.,?!]/g;
+	const cleanItem = item.replace(punctuation, "").toLowerCase();
+	const cleanReversed = cleanItem.split('').reverse().join('');
 
-};
+	if (cleanItem.localeCompare(cleanReversed) === 0) {
+	  return true;
+	} else {
+	  return false;
+	}
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
