89b911ecf4e97f31aac00759efd089347de32650
Pablo Miralles <pablo@mowomo.com>
2024-04-28 19:55:48 +0200

    Completed exercise 8

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -7,5 +7,7 @@ const removeFromArray = function (givenArray, ...elementsToRemove) {
 	return resultArray;
 };
 
+removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
+
 // Do not edit below this line
 module.exports = removeFromArray;
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce( (total, item) => {
+    return total += item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce( (total, item) => {
+    return total *= item;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+
+  if(number === 0) {
+    return 1;
+  }
+
+	const arrayFromNumber = [];
+  for(let i = number; i > 0; i--) {
+    arrayFromNumber.push(i);
+  }
+
+  return arrayFromNumber.reduce( (total, item) => {
+    return total *= item;
+  });
+
 };
 
 // Do not edit below this line
