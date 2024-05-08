796e2d419d13d3fa72ec78a5ea10651a93f7e482
Pablo Miralles <pablo@mowomo.com>
2024-05-08 21:05:55 +0200

    solved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
-
+const fibonacci = function(numberPosition) {
+	numberPosition = +numberPosition;
+	
+	if (numberPosition < 0) {
+		return 'OOPS'
+	} else if (numberPosition === 0) {
+		return 0;
+	} else if (numberPosition === 1) {
+		return 1;
+	} else {
+		let secondToLast = 0;
+		let last = 1;
+		let result;
+		
+		for(let i = 1; i < numberPosition; i++) {
+			result = last + secondToLast;
+		
+			secondToLast = last;
+			last = result;
+		}
+		
+		return result;
+	
+	}
 };
 
 // Do not edit below this line
